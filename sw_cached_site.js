8f18f8c362b1f197902da1c538d21b14c9d5ef0c
Wagner Mattos <wagner_mattos@msn.com>
2018-08-03 16:38:05 -0300

    Version 1

---
+++ sw_cached_site.js
@@ -0,0 +1,48 @@
+const cacheName = 'asSite';
+
+// Call Install Event
+self.addEventListener('install', e => {
+  console.log('[Service Worker]: Installed.');
+});
+
+// Call Activate Event
+self.addEventListener('activate', e => {
+  console.log('[Service Worker]: Activated.');
+  // Remove unwanted caches
+  e.waitUntil(
+    caches.keys()
+    .then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== cacheName) {
+            console.log(`[Service Worker]: Clearing Old Cache named as <${cache}>.`);
+            return caches.delete(cache);
+          }
+        })
+      )
+    })
+  );
+});
+
+// Call Fetch Event
+self.addEventListener('fetch', e => {
+  console.log('[Service Worker]: Fetching.');
+  e.respondWith(
+    fetch(e.request)
+    .then(res => {
+      // Make copy/clone of response
+      const resClone = res.clone();
+      // Open Cache
+      caches
+        .open(cacheName)
+        .then(cache => {
+          // Add reponse to cache
+          cache.put(e.request, resClone);
+        });
+      return res;
+    })
+    .catch(err => caches.match(e.request).then(res => res)
+
+    )
+  );
+});
